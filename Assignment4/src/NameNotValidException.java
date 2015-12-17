7709389035535826301d288e17af84219d810356
Sarbjit <sarbjit.gahra@gmail.com>
2015-12-17 17:00:39 -0500

    Initial commit

---
+++ Assignment4/src/NameNotValidException.java
@@ -0,0 +1,4 @@
+
+public class NameNotValidException extends Exception {
+
+}
---
+++ Assignment4/src/Username.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Username {
+	private String username;
+	
+	
+	
+	
+	public void enterUsername(String username) throws NameNotValidException{
+		this.username = username;
+		if (username.matches(". * \\ d.* ")){
+			throw 
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
